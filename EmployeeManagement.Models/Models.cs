82fb4dc3ced75a51ab6f9b5d1e5bbaef2a699e37
rhrashal <rhrashal@gmail.com>
2022-01-31 12:30:01 +0600

    update stracture

---
+++ EmployeeManagement.Models/Models.cs
@@ -0,0 +1,26 @@
+﻿namespace EmployeeManagement.Models
+{
+    public class Department
+    {
+        public int DepartmentId { get; set; }
+        public string DepartmentName { get; set; } = string.Empty;
+    }
+    public enum Gender
+    {
+        Male,
+        Female,
+        Other
+    }
+
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public DateTime DateOfBrith { get; set; }
+        public Gender Gender { get; set; }
+        public Department Department { get; set; } = new Department();
+        public string PhotoPath { get; set; } = string.Empty;
+    }
+}
